0e69d1386ca0b4841f7422038769d5231cfcf03e
Austin Powell <austinpowell58@gmail.com>
2018-07-28 01:48:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {kesha: "Isaac Song"};
 
 function updatePlaylist(obj, key, value) {
-  return Object.assign( obj, { [key]: value });
+  return Object.assign( {}, obj, { [key]: value });
   }
 function removeFromPlaylist(obj, key) {
   delete obj[key];
